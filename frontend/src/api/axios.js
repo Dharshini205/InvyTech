5fb134d5b115f11144336578a4649ecf9f9b5b0d
Dharshini205 <dharshinivenkat005@gmail.com>
2025-07-25 22:57:45 +0530

    Replaced broken frontend with working React app

--- frontend/src/api/axios.js
+++
@@ -1,9 +0,0 @@
-// src/api/axios.js
-import axios from 'axios';
-
-export default axios.create({
-  baseURL: 'http://localhost:5000', // Backend URL
-  headers: {
-    'Content-Type': 'application/json'
-  }
-});
